51c0db253c3349a6e471450bfc539a05f90db1df
emacias6950-prog <emacias6950@utm.edu.ec>
2026-01-12 00:40:23 -0500

    proyecto tulli

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:3000';
+const API = import.meta.env.VITE_API_URL || 'https://practica-prosgres.onrender.com';
 
 // Elementos Globales
 const loginSection = document.getElementById('login-section');
